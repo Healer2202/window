0baf08e351fcfb9e8fcf4b6d32a3eb77d03af725
NguyenHoangLong <55733418+Healer2202@users.noreply.github.com>
2019-11-14 13:52:44 +0700

    Add files via upload
    14_11_19

--- Program.cs
+++ Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var diary = new OOPCSEntities();
+            var list = diary.Expenditures.ToArray();
+            int sum = 0;
+            for (int i =0; i < list.Length; i++)
+            {
+                sum = sum + list[i].Price;
+                //Console.WriteLine("ID=" + list[i].id, "Date=" + list[i].Date, "Price =" + list[i].Price);
+                Console.WriteLine("Tong chi tieu :" + sum);
+            }
+            diary.Dipose();
         }
     }
 }
